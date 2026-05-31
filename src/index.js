d6e1be014d8ad35e1a14b2fb52ad529ea4f17794
bendover111222333444 <sigmasigmaonthewallwhoisthe2@gmail.com>
2026-05-31 16:36:16 -0700

    inital commit

---
+++ src/index.js
@@ -0,0 +1,84 @@
+import { createServer } from "node:http";
+import { fileURLToPath } from "url";
+import { hostname } from "node:os";
+import Fastify from "fastify";
+import fastifyStatic from "@fastify/static";
+
+import { scramjetPath } from "@mercuryworkshop/scramjet/path";
+import { libcurlPath } from "@mercuryworkshop/libcurl-transport";
+import { baremuxPath } from "@mercuryworkshop/bare-mux/node";
+
+const publicPath = fileURLToPath(new URL("../public/", import.meta.url));
+
+// Wisp Configuration: Refer to the documentation at https://www.npmjs.com/package/@mercuryworkshop/wisp-js
+
+const fastify = Fastify({
+    serverFactory: (handler) => {
+        return createServer()
+            .on("request", (req, res) => {
+                res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
+                res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
+                handler(req, res);
+            });
+    },
+});
+
+fastify.register(fastifyStatic, {
+	root: publicPath,
+	decorateReply: true,
+});
+
+fastify.register(fastifyStatic, {
+	root: scramjetPath,
+	prefix: "/scram/",
+	decorateReply: false,
+});
+
+fastify.register(fastifyStatic, {
+	root: libcurlPath,
+	prefix: "/libcurl/",
+	decorateReply: false,
+});
+
+fastify.register(fastifyStatic, {
+	root: baremuxPath,
+	prefix: "/baremux/",
+	decorateReply: false,
+});
+
+fastify.setNotFoundHandler((res, reply) => {
+	return reply.code(404).type("text/html").sendFile("404.html");
+});
+
+fastify.server.on("listening", () => {
+	const address = fastify.server.address();
+
+	// by default we are listening on 0.0.0.0 (every interface)
+	// we just need to list a few
+	console.log("Listening on:");
+	console.log(`\thttp://localhost:${address.port}`);
+	console.log(`\thttp://${hostname()}:${address.port}`);
+	console.log(
+		`\thttp://${
+			address.family === "IPv6" ? `[${address.address}]` : address.address
+		}:${address.port}`
+	);
+});
+
+process.on("SIGINT", shutdown);
+process.on("SIGTERM", shutdown);
+
+function shutdown() {
+	console.log("SIGTERM signal received: closing HTTP server");
+	fastify.close();
+	process.exit(0);
+}
+
+let port = parseInt(process.env.PORT || "");
+
+if (isNaN(port)) port = 8080;
+
+fastify.listen({
+	port: port,
+	host: "0.0.0.0",
+});
