6a01d5ca239df8786fb0438e656432fd38f6bfe0
bendover111222333444 <sigmasigmaonthewallwhoisthe2@gmail.com>
2026-06-03 19:19:52 -0700

    fx

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,30 @@
 importScripts("/controller.sw.js");
 
 addEventListener("fetch", e => {
-    if ($scramjetController.shouldRoute(e)) {
-        e.respondWith($scramjetController.route(e));
+    if (!$scramjetController.shouldRoute(e)) return;
+
+    let request = e.request;
+
+    try {
+        const url = new URL(e.request.url);
+        if (url.hostname.includes("googlevideo.com")) {
+            url.searchParams.set("c", "MWEB");
+            url.searchParams.delete("sabr");
+            url.searchParams.delete("rqh");
+            request = new Request(url.toString(), {
+                method: e.request.method,
+                headers: e.request.headers,
+                body: e.request.method !== "GET" && e.request.method !== "HEAD"
+                    ? e.request.body
+                    : undefined,
+                mode: e.request.mode,
+                credentials: e.request.credentials,
+                redirect: e.request.redirect,
+            });
+        }
+    } catch (err) {
+        console.error("[sw] Failed to rewrite googlevideo request:", err);
     }
+
+    e.respondWith($scramjetController.route({ ...e, request }));
 });
